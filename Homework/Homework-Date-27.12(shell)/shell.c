8d68021b3bbea8741973de631568c2e6e0d3a48e
petardmnv <petar.dmnv@gmail.com>
2020-12-28 22:38:30 +0200

    Add solution for my homework - shell

--- Homework/Homework-Date-27.12(shell)/shell.c
+++ Homework/Homework-Date-27.12(shell)/shell.c
@@ -5,7 +5,6 @@
 // PROBLEM: #1
 // FILE NAME: shell.c
 // FILE PURPOSE: Program that implements shell interpreter
-// ...
 //------------------------------------------------------------------------
 
 
@@ -18,6 +17,7 @@
 #include <errno.h>
 #include <string.h>
 
+
 //------------------------------------------------------------------------
 // FUNCTION: **parse_cmdline
 // converts C-string to C-strings
@@ -25,53 +25,43 @@
 // 
 //------------------------------------------------------------------------
 char **parse_cmdline(const char *cmdline){
-	char split_by[3] = " \n";
 
-	char *non_const_cmdline = malloc(strlen(cmdline));
+	int cmdline_length = strlen(cmdline);
+	char *non_const_cmdline = malloc(cmdline_length);
+
 	strcpy(non_const_cmdline, cmdline);
 
-	//15ti red e tumna indiq:)
 	char **parsed_string = (char**) malloc(sizeof(char*));
-	int parsing_count = 0;
+	int parsing_count = 1;
 
 	char* token;
-	token = strtok(non_const_cmdline, split_by);
+	token = strtok(non_const_cmdline, " \n");
 	while (token != NULL){
+		//That is usless :(
+		int token_length = strlen(token) + 1;
+		parsed_string[parsing_count - 1] = malloc(token_length);
+		strncpy(parsed_string[parsing_count - 1], token, token_length);
+		token = strtok(NULL, " \n");
 		parsing_count += 1;
-
-		parsed_string = (char**) realloc(parsed_string, sizeof(char*) * parsing_count);
-		parsed_string[parsing_count - 1] = malloc(strlen(token));
-		parsed_string[parsing_count - 1] = token;
-
-		token = strtok(NULL, split_by);
+		parsed_string = (char**) realloc(parsed_string, sizeof(char*) * (parsing_count + 1));
 	}
 
-	parsed_string = (char**) realloc(parsed_string, sizeof(char*) * (parsing_count + 1));
-	parsed_string[parsing_count] = malloc(sizeof(NULL));
 	parsed_string[parsing_count] = NULL;
-
+	free(non_const_cmdline);
 	return parsed_string;
-}
-
-
-//------------------------------------------------------------------------
-// FUNCTION: print_dollar
-// prints dollar sigh
-// PARAMETERS: N/A
-// 
-//------------------------------------------------------------------------
-void print_dollar(){
-	char dollar_sign[3] = "$ ";
-	write(STDOUT_FILENO, &dollar_sign, 2);
 }
 
 int main(int argc, char const *argv[]){
 	while(1){
-		print_dollar();
+		char *dollar_sign = "$ ";
+		if ((write(STDOUT_FILENO, dollar_sign, 2)) < 0){
+			perror("write");
+			return -1;
+		}
 		size_t b = 0;
 		char *buff = NULL;
 		if ((getline(&buff, &b, stdin)) == -1){
-			perror("read");
+			free(buff);
 			return -1;
 		}
 		pid_t pid;
@@ -88,15 +78,15 @@ int main(int argc, char const *argv[]){
 				perror(arguments[0]);
 				exit(-1);
 			}
+			free(arguments);
 			exit(0); 
 		}else {
 			if (waitpid(pid, &status, 0) != pid){
-				status = -1;
-			}
-			if (status == -1){
 				perror("waitpid");
+				return -1;
 			}
 		}
+		free(buff);
 	}
 	return 0;
 }
