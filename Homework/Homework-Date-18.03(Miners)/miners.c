eb1da3e7e1fb248d7e344e63bd6c621d024a19df
petardmnv <petar.dmnv@gmail.com>
2021-03-18 13:31:36 +0200

    Add Miners

--- Homework/Homework-Date-18.03(Miners)/miners.c
+++ Homework/Homework-Date-18.03(Miners)/miners.c
@@ -1,3 +1,10 @@
+//--------------------------------------------
+// NAME: Petur Damianov
+// CLASS: XI b
+// NUMBER: 21
+// PROBLEM: Miners
+//---------------------------------------------
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,30 +19,52 @@
 int Gold = 0;
 
 pthread_mutex_t lock; 
+
 /* THREAD 1 - miner*/
-void *get_gold(void *p){
-    //read from terminal
-    pthread_mutex_lock(&lock);
-    int *miner_id = (int*) p;
-    Gold += 10;
-    printf("Miner %d gathered 10 gold\n", *miner_id);
-    pthread_mutex_unlock(&lock);
-    sleep(2);
+void* get_gold(void *id){
+    int miner_id = *(int*) id;
+    for (int i = 0; i < 20; i++){
+
+    	if (pthread_mutex_lock(&lock)) { 
+			printf("\n ERROR: mutex_lock() has failed!\n"); 
+			exit(-1); 
+		}
+    	
+        Gold += 10;
+	    printf("Miner %d gathered 10 gold\n", miner_id);
+
+	   	if (pthread_mutex_unlock(&lock)) { 
+			printf("\n ERROR: mutex_unlock() has failed!\n"); 
+			exit(-1); 
+		}
+	    sleep(2);
+    }
     return NULL;
 }
 
 /* THREAD 2 - merchant*/
-void* sell_gold(void *p){
-    pthread_mutex_lock(&lock);
-	int *seller_id = (int*) p;
-    if (Gold >= 10) {
-    	Gold -= 10;
-    	printf("Trader %d sold 10 gold\n", *seller_id);
-    }else {
-    	printf("The warehouse is empty, cannot sell!\n");
-    }
-    pthread_mutex_unlock(&lock);
-    sleep(2);
+void* sell_gold(void *id){
+	int seller_id = *(int*) id;
+	for (int i = 0; i < 20; i++){
+
+		if (pthread_mutex_lock(&lock)) { 
+			printf("\n ERROR: mutex_lock() has failed!\n"); 
+			exit(-1);  
+		}
+
+	    if (Gold >= 10) {
+	    	Gold -= 10;
+	    	printf("Trader %d sold 10 gold\n", seller_id);
+	    }else {
+	    	printf("The warehouse is empty, cannot sell!\n");
+	    }
+
+	   	if (pthread_mutex_unlock(&lock)) { 
+			printf("\n ERROR: mutex_unlock() has failed!\n"); 
+			exit(-1); 
+		}
+	    sleep(2);
+	}
     return NULL;
 }
 
@@ -43,59 +72,67 @@ void* sell_gold(void *p){
 int main(int argc, char *argv[])
 {
 
-	/*
+	int miners_count;
+	int merchants_count;
 
-	After every mined or selled gold people are resting for 2 seconds - sleep()
-
-	Every miner produces 10kg gold
-	Every seller is selliong 10kg gold
-
-	Every mine has current gold resourse
-
-	if there is no more gold and seller wants to sell gold print - "The warehouse is empty, cannot sell!"
-
-	After every action you print - "Miner Ngathered 10 gold" and "Trader N sold 10 gold"
-
-	Afret 20 minings and selling program ends and prints current gold 
-	
-	*/
-
-	int returned_value;
-
-	if (pthread_mutex_init(&lock, NULL) != 0) { 
-		printf("\n mutex init has failed\n"); 
-		return 1; 
+	// Mutex creation
+	if (pthread_mutex_init(&lock, NULL)) { 
+		printf("\n ERROR: mutex_init() has failed!\n"); 
+		exit(-1); 
 	}
-	int miners_count = 1;
-	int merchants_count = 1;
 
 	// Find miners and merchants count
 	if (argc == 3){
 		miners_count = atoi(argv[1]);
 		merchants_count = atoi(argv[2]);
+	}else if (argc == 1){
+		miners_count = 1;
+		merchants_count = 1;
+	}else{
+		printf("\nError: Invalid number of arguments!\n");
+		return -1;
 	}
 
 	pthread_t miner[miners_count], merchant[merchants_count];
 
-    for (int m = 0; m < miners_count; ++m){
-		returned_value = pthread_create(&miner, NULL, &get_gold, &m);
-		if(returned_value){
-	        printf("ERROR: miner pthread_create() return %d - %s \n", returned_value, strerror(returned_value));
+	// Creating threads
+	for (int m = 0; m < miners_count; m++){
+		int miner_id = m + 1;
+		if(pthread_create(&(miner[m]), NULL, &get_gold, &miner_id)){
+	        printf("\n ERROR: miner pthread_create() has failed!\n");
 	       	return 1;
 	    }
-    }
+	}
 
-    for (int t = 0; t < merchants_count; ++t){
-       	returned_value = pthread_create(&merchant, NULL, &sell_gold, &t);
-		if(returned_value){
-		   	printf("ERROR: merchant pthread_create() return %d - %s \n", returned_value, strerror(returned_value));
+
+    for (int t = 0; t < merchants_count; t++){
+    	int merchant_id = t + 1;
+		if(pthread_create(&(merchant[t]), NULL, &sell_gold, &merchant_id)){
+		   	printf("\n ERROR: merchant pthread_create() has failed!\n");
 		    return 1;
 		}
     }
 
-    //TODO multiple threads joining
-    pthread_join(miner, NULL);
-    pthread_join(merchant, NULL);
+    // Joining threads
+	for (int i = 0; i < miners_count; i++){
+		if (pthread_join(miner[i], NULL)) { 
+			printf("\n ERROR: miner pthread_join() has failed!\n"); 
+			exit(-1); 
+		}	
+	}
+	for (int i = 0; i < merchants_count; i++){
+		if (pthread_join(merchant[i], NULL)) { 
+			printf("\n ERROR: merchant pthread_join() has failed!\n"); 
+			exit(-1); 
+		}	
+	}
+
+
+	// Mutex destroing
+	if (pthread_mutex_destroy(&lock)) { 
+		printf("\n ERROR: mutex_destroy() has failed!\n"); 
+		exit(-1); 
+	}
 
     printf("Gold: %d\n", Gold);
     return 0;
